228590caedf81923a02de2d97c3578a48e45e81e
Ciro Cavani <ciro.cavani@gmail.com>
2013-07-30 23:49:03 -0300

    Sample: cache events for eviction (WIP)

--- sample/cache.go
+++ sample/cache.go
@@ -6,19 +6,50 @@ import (
 	"log"
 	"runtime"
 	"sync"
-	_ "time"
+	"time"
 )
 
 var optThreads = flag.Int("threads", runtime.NumCPU(), "Number of system threads")
 
+type EventType int
+
+const (
+	SET EventType = iota
+	DEL
+)
+
+type Event struct {
+	Type      EventType
+	Key       string
+	Timestamp time.Time
+}
+
+func (this *Event) String() string {
+	return fmt.Sprintf("%v, %s, %v", this.Type, this.Key, this.Timestamp)
+}
+
 type Cache struct {
 	sync.RWMutex
 	data map[string]interface{}
+
+	event chan *Event
 }
 
-func NewCache() *Cache {
+func NewCache(ttl time.Duration, size int) *Cache {
+	event := make(chan *Event, 100)
+
+	eviction := func() {
+		for {
+			e := <-event
+			fmt.Println(e)
+		}
+	}
+
+	go eviction()
+
 	return &Cache{
-		data: make(map[string]interface{}),
+		data:  make(map[string]interface{}),
+		event: event,
 	}
 }
 
@@ -52,9 +83,19 @@ func (this *Cache) Get(key string) interface{} {
 	return value
 }
 
+func (this *Cache) riseEvent(t EventType, keys ...string) {
+	timestamp := time.Now()
+	go func() {
+		for _, key := range keys {
+			this.event <- &Event{t, key, timestamp}
+		}
+	}()
+}
+
 func (this *Cache) Set(key string, value interface{}) interface{} {
 	this.Lock()
 	defer this.Unlock()
+	defer this.riseEvent(SET, key)
 	this.data[key] = value
 	return value
 }
@@ -62,6 +103,7 @@ func (this *Cache) Set(key string, value interface{}) interface{} {
 func (this *Cache) Delete(keys ...string) {
 	this.Lock()
 	defer this.Unlock()
+	defer this.riseEvent(DEL, keys...)
 	for _, key := range keys {
 		delete(this.data, key)
 	}
@@ -75,7 +117,10 @@ func main() {
 	fmt.Println("Threads:", *optThreads)
 	runtime.GOMAXPROCS(*optThreads)
 
-	cache := NewCache()
+	ttl := 5 * time.Second
+	size := 10
+
+	cache := NewCache(ttl, size)
 	cache.Set("x", []int{1, 2, 3})
 	cache.Set("y", "[1 2 3]")
 
@@ -84,4 +129,6 @@ func main() {
 	cache.Delete("y")
 
 	fmt.Println(cache)
+
+	time.Sleep(10 * time.Second)
 }
